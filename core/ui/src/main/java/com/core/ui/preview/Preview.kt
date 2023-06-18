27cb87833124e0201c88b8317b4329513c586e6f
Vladyslav Storchevyi <v.storchevyi@gmail.com>
2023-06-18 18:06:46 +0300

    Split ThemePreviews into the groups (Light, Dark) for better preview usability

--- core/ui/src/main/java/com/core/ui/preview/Preview.kt
+++ core/ui/src/main/java/com/core/ui/preview/Preview.kt
@@ -3,6 +3,6 @@ package com.core.ui.preview
 import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light theme")
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark theme")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light theme", group = "Light")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark theme", group = "Dark")
 annotation class ThemePreviews
